c41dc53c8fddd3cbd2c0de6e6496541bd3b3c910
Tomke Schmitt <deine@email.com>
2026-03-12 18:35:29 +0100

    Projekt-Setup

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -13,8 +13,7 @@ export async function GET(req: Request, { params }: paramsType) {
         user: {
           select: {
             id: true,
-            name: true,
-            image: true
+            name: true
           }
         }
       }
